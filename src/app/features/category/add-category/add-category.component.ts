11f6d5d95a3748895e96ec0e0d99e750844b5e4a
Szymon Fornal <szymekfornal6@gmail.com>
2024-06-27 14:22:51 +0200

    Create Add category

---
+++ src/app/features/category/add-category/add-category.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnDestroy } from '@angular/core';
+import { AddCategoryRequest } from '../models/add-category-request.model';
+import { CategoryService } from '../../services/category.service';
+import { Subscription } from 'rxjs';
+@Component({
+  selector: 'app-add-category',
+  templateUrl: './add-category.component.html',
+  styleUrls: ['./add-category.component.css'],
+})
+export class AddCategoryComponent implements OnDestroy {
+  model: AddCategoryRequest;
+  private addCategorySubscribtion?: Subscription;
+
+  constructor(private CategoryService: CategoryService) {
+    this.model = {
+      name: '',
+      urlHandle: '',
+    };
+  }
+
+  onFormSubmit() {
+    this.addCategorySubscribtion = this.CategoryService.addCategory(
+      this.model
+    ).subscribe({
+      next: (response) => {
+        console.log('This was successful');
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.addCategorySubscribtion?.unsubscribe();
+  }
+}
