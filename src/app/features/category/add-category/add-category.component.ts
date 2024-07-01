558a00773592efcef1a8350190ef147ead84f22e
Szymon Fornal <szymekfornal6@gmail.com>
2024-07-01 12:24:24 +0200

    CRUD for category

--- src/app/features/category/add-category/add-category.component.ts
+++ src/app/features/category/add-category/add-category.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { CategoryService } from '../../services/category.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
@@ -11,7 +12,10 @@ export class AddCategoryComponent implements OnDestroy {
   model: AddCategoryRequest;
   private addCategorySubscribtion?: Subscription;
 
-  constructor(private CategoryService: CategoryService) {
+  constructor(
+    private CategoryService: CategoryService,
+    private router: Router
+  ) {
     this.model = {
       name: '',
       urlHandle: '',
@@ -23,7 +27,7 @@ export class AddCategoryComponent implements OnDestroy {
       this.model
     ).subscribe({
       next: (response) => {
-        console.log('This was successful');
+        this.router.navigateByUrl('/admin/categories');
       },
     });
   }
