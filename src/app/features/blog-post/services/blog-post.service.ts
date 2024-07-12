dedbe60f7bc4027abdaf5f05fbcfc7e991aeaf13
Szymon Fornal <szymekfornal6@gmail.com>
2024-07-12 15:24:47 +0200

    Added Auth header and Auth Guard

--- src/app/features/blog-post/services/blog-post.service.ts
+++ src/app/features/blog-post/services/blog-post.service.ts
@@ -14,7 +14,7 @@ export class BlogPostService {
 
   createBlogPost(data: AddBlogPost): Observable<BlogPost> {
     return this.http.post<BlogPost>(
-      `${environment.apiBaseUrl}/api/blogposts`,
+      `${environment.apiBaseUrl}/api/blogposts?addAuth=true`,
       data
     );
   }
@@ -34,14 +34,14 @@ export class BlogPostService {
     updatedBlogPost: UpdateBlogPost
   ): Observable<BlogPost> {
     return this.http.put<BlogPost>(
-      `${environment.apiBaseUrl}/api/blogposts/${id}`,
+      `${environment.apiBaseUrl}/api/blogposts/${id}?addAuth=true`,
       updatedBlogPost
     );
   }
 
   deleteBlogPost(id: string): Observable<BlogPost> {
     return this.http.delete<BlogPost>(
-      `${environment.apiBaseUrl}/api/blogposts/${id}`
+      `${environment.apiBaseUrl}/api/blogposts/${id}?addAuth=true`
     );
   }
 
